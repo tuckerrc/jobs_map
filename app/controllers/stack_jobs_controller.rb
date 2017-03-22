9458148774e1f4d246bbdf4cbc691fb8762eb11a
Tucker <tucker.r.chapman@gmail.com>
2017-03-21 21:28:07 -0600

    Add new search fields

--- app/controllers/stack_jobs_controller.rb
+++ app/controllers/stack_jobs_controller.rb
@@ -1,6 +1,9 @@
 class StackJobsController < ApplicationController
   def index
     @search_term = params[:search] || 'ruby on rails'
-    @stack_jobs = StackJob.for @search_term
+    @min_experience = params[:min] || ''
+    @max_experience = params[:max] || ''
+    @job_type = params[:type] || ''
+    @stack_jobs = StackJob.for( @search_term, @min_experience, @max_experience, @job_type )
   end
 end
--- app/models/stack_job.rb
+++ app/models/stack_job.rb
@@ -2,8 +2,13 @@ class StackJob
   require 'active_support/core_ext/hash/conversions'
   require 'pp'
 
-  def self.for term
-    url = "http://stackoverflow.com/jobs/feed?q=#{term}&l=United%20States&d=20&u=Miles"
+  def self.for( term, min_ex, max_ex, job_type )
+    url = "http://stackoverflow.com/jobs/feed?"\
+          "q=#{term}"\
+          "&ms=#{min_ex}"\
+          "&mxs=#{max_ex}"\
+          "&j=#{job_type}"\
+          "&l=United%20States&d=20&u=Miles" # Currently the application only supports US
     xml = Nokogiri::XML(open(url))
     hash = Hash.from_xml(xml.to_s)
     hash["rss"]["channel"]["item"]
