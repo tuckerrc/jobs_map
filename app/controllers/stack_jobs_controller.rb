161b82951958af635d10c5b2bc38376d9924bda4
Tucker Chapman <tucker.r.chapman@gmail.com>
2018-04-12 21:06:24 -0400

    Start work on to_geojson method for Stack Jobs

--- app/controllers/stack_jobs_controller.rb
+++ app/controllers/stack_jobs_controller.rb
@@ -1,9 +1,10 @@
 class StackJobsController < ApplicationController
   def index
-    @search_term = params[:search] || '[ruby-on-rails]'
+    @search_term = params[:search] || '[gis]'
     @min_experience = params[:min] || ''
     @max_experience = params[:max] || ''
     @job_type = params[:type] || ''
-    @stack_jobs = StackJob.for( @search_term, @min_experience, @max_experience, @job_type )
+
+    @stack_jobs = StackJob.new( @search_term, @min_experience, @max_experience, @job_type )
   end
 end
--- app/models/stack_job.rb
+++ app/models/stack_job.rb
@@ -1,16 +1,39 @@
 class StackJob
+  attr_accessor :url, :xml, :geojson
+
   require 'active_support/core_ext/hash/conversions'
   require 'pp'
 
-  def self.for( term, min_ex, max_ex, job_type )
-    url = "https://stackoverflow.com/jobs/feed?"\
+  def initialize( term, min_ex, max_ex, job_type )
+    @url = "https://stackoverflow.com/jobs/feed?"\
           "q=#{term}"\
           "&ms=#{min_ex}"\
           "&mxs=#{max_ex}"\
           "&j=#{job_type}"\
           "&l=United%20States&d=20&u=Miles" # Currently the application only supports US
-    xml = Nokogiri::XML(open(url))
-    hash = Hash.from_xml(xml.to_s)
-    hash["rss"]["channel"]["item"]
+    @xml = Nokogiri::XML(open(url))
+    self.to_geojson
+    @geojson
+  end
+
+  def to_geojson
+      hash = Hash.from_xml(@xml.to_s)
+      items = hash['rss']['channel']['item']
+      json = '{ "type": "FeatureCollection","features" : ['
+      items.each do | item |
+          json << '{"type": "Feature", "properties": {'
+          json << '"show_on_map" : true,'
+          json << '"name" : "' << item['title'] << '",'
+          json << '"link" : "' << item['link'] << '",'
+          json << '"company" : "' << item['author']['name'] << '",'
+          json << '"city" : "' << item['location'] << '",'
+          json << '"date" : "' << item['pubDate'] << '",'
+
+          json << '},'
+      end
+      json = json[0...-1]
+      json << ']}'
+      @geojson = json
+      puts @geojson
   end
 end
