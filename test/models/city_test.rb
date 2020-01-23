ab87eb0dde17f98ff413cdc11b26118af84aadad
Tucker Chapman <tucker.r.chapman@gmail.com>
2020-01-22 21:29:13 -0500

    Add cities tests

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "find one" do
+    assert_equal "new york", cities(:new_york).name
+  end
+
+  test "split_city_state" do
+    split = City.split_city_state("new york, ny")
+    assert_equal "new york", split[0]
+    assert_equal "ny", split[1]
+  end
+
+  test "find_coordinates" do
+    coord = City.find_coordinates("new york", "ny")
+    assert_equal 40.71427, coord.lat
+    assert_equal (-74.00597) , coord.long
+  end
 end
