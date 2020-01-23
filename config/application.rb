24749d54e67bf0965681b0e33ae3dfc66a2e3e2f
Tucker Chapman <tucker.r.chapman@gmail.com>
2020-01-22 21:18:18 -0500

    Fix deprecation error for sqlite boolean changes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module JobSearchMap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
