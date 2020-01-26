be7e8839e860fff57506141047417b8a5605904a
Tucker Chapman <tucker.r.chapman@gmail.com>
2020-01-25 21:12:36 -0500

    Add stack job model tests

--- test/models/stack_job_test.rb
+++ test/models/stack_job_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StackJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "to_geojson" do
+    assert_equal '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"show_on_map":"true","link":"https://example.com/link","name":"title (city, st)","company":"Author","city":"City, ST","category":["ruby","postgresql","javascript","ruby-on-rails"],"date":"2020-01-06T21:37:34"},"geometry":{"type":"Point","coordinates":[-71.68,29.85]}},{"type":"Feature","properties":{"show_on_map":"true","link":"https://example.com/link","name":"title (city, st)","company":"Author","city":"City, ST","category":["ruby","postgresql","javascript","ruby-on-rails"],"date":"2020-01-06T21:37:34"},"geometry":{"type":"Point","coordinates":[-71.68,29.85]}}]}', stack_jobs(:test_jobs).to_geojson
+  end
 end
