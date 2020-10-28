7d607dea370a3fe5b41349c8185fed9054df27da
Tucker Chapman <tucker.r.chapman@gmail.com>
2020-10-27 21:35:01 -0400

    Add stack jobs search tests

--- test/controllers/stack_jobs_controller_test.rb
+++ test/controllers/stack_jobs_controller_test.rb
@@ -6,4 +6,14 @@ class StackJobsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get search" do
+    get search_url
+    assert_response :success
+  end
+
+  test "should get search with params" do
+    get search_url params: {"search": "[javascript]"}
+    assert_response :success
+  end
+
 end
